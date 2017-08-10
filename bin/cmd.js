d3d36489431fb052ca2a0e18661d6eacaa4d991f
David Llop <llop-no-spam-please@protonmail.com>
2017-08-10 02:45:05 +0200

    Add support for time offset in the format hh:mm:ss,mmm

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,7 +4,7 @@ var resync = require('../index')
 var fs = require('fs')
 
 function printUsage () {
-  console.error('USAGE: resync-srt [file] millisecond-offset')
+  console.error('USAGE: resync-srt [file] (hh:mm:ss,mmm | millisecond-offset)')
 }
 
 if (process.argv.length < 3 || process.argv.length > 4) {
@@ -20,12 +20,29 @@ if (process.argv.length === 4) {
   msArgIdx = 3
 }
 
-// parse milliseconds arg
-var msOffset = parseInt(process.argv[msArgIdx], 10)
-if (isNaN(msOffset)) {
-  console.error('Invalid millisecond-offset value. Must be numeric.\n')
-  printUsage()
-  process.exit(1)
+// parse offset arg
+var match = process.argv[msArgIdx].match(/^(\-?)(\d{2}):(\d{2}):(\d{2}),(\d{3})$/)
+
+if (match) {
+  var negative = !!match[1]
+  var hours = parseInt(match[2], 10)
+  var minutes = parseInt(match[3], 10)
+  var seconds = parseInt(match[4], 10)
+  var milliseconds = parseInt(match[5], 10)
+
+  hours *= 3600000
+  minutes *= 60000
+  seconds *= 1000
+
+  msOffset = hours + minutes + seconds + milliseconds
+  msOffset *= (negative ? -1 : 1)
+} else {
+  var msOffset = parseInt(process.argv[msArgIdx], 10)
+  if (isNaN(msOffset)) {
+    console.error('Invalid offset value. Must be numeric or using the format hh:mm:ss,mmm.\n')
+    printUsage()
+    process.exit(1)
+  }
 }
 
 stream.pipe(resync(msOffset)).pipe(process.stdout)
